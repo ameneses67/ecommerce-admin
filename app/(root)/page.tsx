3da41c8eaef4b1f5a6670126a1aee2d54480a942
Poncho <ameneses67@gmail.com>
2023-11-11 12:34:34 -0600

    Admin environment setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<Button
+			size="lg"
+			variant="secondary"
+		>
+			Click Me
+		</Button>
+	);
+}
