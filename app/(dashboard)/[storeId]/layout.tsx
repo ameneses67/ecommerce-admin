a510048bf1363c7fea7add7360592923c19d0f9d
Poncho <ameneses67@gmail.com>
2023-11-15 23:22:50 -0600

    Admin navigation bar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
 
 interface Props {
 	children: React.ReactNode;
@@ -29,7 +30,7 @@ export default async function DashboardLayout({ children, params }: Props) {
 
 	return (
 		<>
-			<div>This will be a Navbar</div>
+			<Navbar />
 			{children}
 		</>
 	);
